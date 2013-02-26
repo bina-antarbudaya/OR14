fb13523cf40f609f60df0ae62296d081d2463a14
Andhika Nugraha <andhika.nugraha@gmail.com>
2013-02-26 17:23:53 +0700

    Updated header look and feel
    * Cleaned up code
    * Added form transcript download button for confirmed applicants

--- app/views/global/header.php
+++ app/views/global/header.php
@@ -44,21 +44,32 @@ $action_css = $controller . '/' . $this->_action();
 								echo $this->session->user->capable_of('chapter_admin') ?
 									$this->user->chapter->get_title() :
 									$this->session->user->username;
-							?></li>
+								?>
+							</li>
 							<?php /* TODO clean this up */ ?>
 							<li class="continue">
-								<i class="icon-<?php
-								echo
-								$this->session->user->capable_of('chapter_admin') ?
-									'home' :
-									((!$this->can_register() || $this->user->applicant->confirmed) ? 'certificate' : 'pencil')
-								?> icon-white"></i>
-								<a href="<?php L($this->is_logged_in() ? $this->session->user->get_landing_page() : ''); ?>"><?php
-								echo
-								$this->session->user->capable_of('chapter_admin') ?
-									'Dashboard' :
-									((!$this->can_register() || $this->user->applicant->confirmed) ? 'Pengumuman' : 'Pendaftaran')
-								?></a>
+							<?php if ($this->session->user->capable_of('chapter_admin')): ?>
+								<i class="icon-home icon-white"></i>
+								<a href="<?php L($this->session->user->get_landing_page()); ?>">
+									Dashboard
+								</a>
+							<?php elseif ($this->user->applicant->confirmed): ?>
+								<i class="icon-certificate icon-white"></i>
+								<a href="<?php L($this->session->user->get_landing_page()); ?>">
+									Pengumuman
+								</a>
+							</li>
+							<li>
+								<i class="icon-download-alt icon-white"></i>
+								<a href="<?php L(array('controller' => 'applicant', 'action' => 'transcript')) ?>" data-toggle="dropdown">
+									Transkrip Formulir
+								</a>
+							<?php else: ?>
+								<i class="icon-pencil icon-white"></i>
+								<a href="<?php L($this->session->user->get_landing_page()); ?>">
+									Formulir Pendaftaran
+								</a>
+							<?php endif; ?>
 							</li>
 							<li class="prefs">
 								<i class="icon-cog icon-white"></i> <a href="<?php L(array('controller' => 'user', 'action' => 'prefs')); ?>">Pengaturan</a>
