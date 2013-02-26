078b3327c32c6d7b152e99b50638e3ec4249ee2f
Andhika Nugraha <andhika.nugraha@gmail.com>
2013-02-26 17:22:48 +0700

    Updated applicant list

--- app/views/chapter/applicants/list.php
+++ app/views/chapter/applicants/list.php
@@ -18,7 +18,9 @@
 	<thead>
 		<tr>
 			<th class="checkbox"></th>
+			<?php if ($current_stage != 'expired'): ?>
 			<th class="test-id">No. Peserta</th>
+			<?php endif; ?>
 			<th class="name">Nama Lengkap</th>
 			<th class="school">Asal Sekolah</th>
 			<th class="expires-on">Batas Pendaftaran</th>
@@ -34,7 +36,9 @@
 	?>
 		<tr class="<?php echo $classes; ?>">
 			<td class="checkbox"></td>
+			<?php if ($current_stage != 'expired'): ?>
 			<td class="test-id"><?php echo $a->test_id; ?></td>
+			<?php endif; ?>
 			<td class="name"><b><a href="<?php L(array('controller' => 'applicant', 'action' => 'view', 'id' => $a->id)) ?>"><?php echo $a->sanitized_full_name ? $a->sanitized_full_name : '<span class="empty">(Belum diisi)</span>'; ?></a></b></td>
 			<td class="school"><?php echo $a->sanitized_high_school_name; ?></td>
 			<td class="expires-on"><?php echo $exp->format('j F Y'); ?></td>
