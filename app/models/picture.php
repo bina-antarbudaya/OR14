ca73d4fb7dcffb84d42b5f8cd62f105ae1cee8ba
Andhika Nugraha <andhika.nugraha@outlook.com>
2014-02-03 20:58:34 +0000

    Updated Picture class
    Now supports Azure

--- app/models/picture.php
+++ app/models/picture.php
@@ -10,6 +10,11 @@
 
 // all files are saved under the path defined in conf picture_upload_path
 
+require __DIR__ . '/../vendor/autoload.php';
+
+use WindowsAzure\Common\ServicesBuilder;
+use WindowsAzure\Common\ServiceException;
+
 class Picture extends HeliumRecord {
 	
 	public $upload_path = '';
@@ -22,6 +27,8 @@ class Picture extends HeliumRecord {
 	public $cropped_filename;
 	public $thumbnail_filename;
 	public $format;
+	public $original_width;
+	public $original_height;
 	public $cropped_width;
 	public $cropped_height;
 	public $processed = false;
@@ -29,14 +36,20 @@ class Picture extends HeliumRecord {
 
 	public $upload_error = '';
 
+	// Azure internal variables
+	private $use_azure;
+	private $blobRestProxy;
+
 	public function init() {
 		$this->upload_path = Helium::conf('picture_upload_path');
 		$this->public_path = Helium::conf('picture_public_path');
 		// $this->belongs_to('applicant');
+
+		$this->use_azure = Helium::conf('use_azure_storage');
 	}
 
 	public function generate_filename() {
-		return sha1(microtime()) . '_' . mt_rand();
+		return sha1(microtime()) . '_' . sha1(mt_rand());
 	}
 
 	public function upload_original($file_array) { // $file_array is $_FILE['userfile']
@@ -48,32 +61,62 @@ class Picture extends HeliumRecord {
 			return false;
 		}
 
+		// Find out the image dimensions and type
+		list($original_width, $original_height, $type) = getimagesize($tmp_name);
+
+		$format = image_type_to_extension($type, false);
+
 		// check format validity
 		// let's just check extensions here
-		$ext = strrchr($name, '.');
-		$ext = substr($ext, 1);
-		$ext = strtolower($ext);
-		if (!in_array($ext, $this->allowed_types)) {
+		if (!in_array($format, $this->allowed_types)) {
 			$this->upload_error = 'invalid_format';
 			return false;
 		}
 
-		$filename = $this->generate_filename() . '.' . $ext;
+		$filename = $this->generate_filename() . '.' . $format;
 
-		// move along
-		$destination = $this->upload_path . '/' . $filename;
-		$move = move_uploaded_file($tmp_name, $destination);
+		// Move the uploaded file to persistent storage
+		$move = $this->move_uploaded_file($tmp_name, $filename);
 		if (!$move) {
 			$this->upload_error = 'failed_move';
 			return false;
 		}
 
-		// success! store our work in the db.
+		// Success!
 
 		$this->original_filename = $filename;
-		$this->format = $ext;
+		$this->format = $format;
+		$this->original_width = $original_width;
+		$this->original_height = $original_height;
 
 		return true;
+	}
+
+	public function get_source_image($filename) {
+		if ($this->use_azure) {
+			$blobRestProxy = $this->getBlobRestProxy();
+			$container = Helium::conf('azure_storage_picture_container');
+
+			try {
+				$blob = $blobRestProxy->getBlob($container, $filename);
+				$length = $blob->getProperties()->getContentLength();
+				$contents = fread($blob->getContentStream(), $length);
+				$source = @imagecreatefromstring($contents);
+			}
+			catch (ServiceException $e) {
+				$code = $e->getCode();
+				$error_message = $e->getMessage();
+				echo $code . ': ' . $error_message;
+				return false;
+			}
+		}
+		else {
+			$full_path = $this->upload_path . '/' . $filename;
+			$contents = file_get_contents($full_path);
+			$source = @imagecreatefromstring($contents);
+		}
+
+		return $source;
 	}
 
 	public function crop($params = array()) {
@@ -92,8 +135,9 @@ class Picture extends HeliumRecord {
 
 		$ideal = $this->get_default_crop_dimensions();
 
-		$original_full_path = $this->upload_path . '/' . $this->original_filename;
-		list($mw, $mh, $type) = getimagesize($original_full_path);
+		$mw = $this->original_width;
+		$mh = $this->original_height;
+		$format = $this->format;
 
 		$checks = array();
 		$checks[] = $params['x'] <= $mw;
@@ -121,20 +165,9 @@ class Picture extends HeliumRecord {
 			$end_height = $max_height;
 		}
 
-		// begin crop
-		switch ($type) {
-			case IMAGETYPE_JPEG:
-				$source = @imagecreatefromjpeg($original_full_path);
-				break;
-			case IMAGETYPE_PNG:
-				$source = @imagecreatefrompng($original_full_path);
-				break;
-			case IMAGETYPE_GIF:
-				$source = @imagecreatefromgif($original_full_path);
-				break;
-			default:
-				return false;
-		}
+		$source = $this->get_source_image($this->original_filename);
+
+		// Initiate canvas
 		$canvas = imagecreatetruecolor($end_width, $end_height);
 
 		// paint to canvas
@@ -146,7 +179,7 @@ class Picture extends HeliumRecord {
 		$cropped_filename = $raw_filename . '_cropped.jpg';
 		$this->cropped_filename = $cropped_filename;
 
-		$save = @imagejpeg($canvas, $this->upload_path . '/' . $cropped_filename);
+		$save = $this->save_image($canvas, $cropped_filename);
 		if (!$save)
 			return false;
 
@@ -155,7 +188,7 @@ class Picture extends HeliumRecord {
 		$this->save();
 		
 		// delete the original image. bye!
-		@unlink($this->upload_path . '/' . $this->original_filename);
+		$this->delete_image($this->original_filename);
 		return true;
 	}
 	
@@ -163,7 +196,7 @@ class Picture extends HeliumRecord {
 		if (!$this->cropped_filename)
 			return false;
 
-		$source = @imagecreatefromjpeg($this->upload_path . '/' . $this->cropped_filename);
+		$source = $this->get_source_image($this->cropped_filename);
 		$canvas = @imagecreatetruecolor(150, 200); // thumbnail is 150x200
 		
 		// this is enough
@@ -175,11 +208,80 @@ class Picture extends HeliumRecord {
 		$thumbnail_filename = $raw_filename . '_thumb.jpg';
 		$this->thumbnail_filename = $thumbnail_filename;
 
-		$save = @imagejpeg($canvas, $this->upload_path . '/' . $thumbnail_filename);
+		$save = $this->save_image($canvas, $thumbnail_filename);
 		if (!$save)
 			return false;
 
 		$this->save();
+
+		return true;
+	}
+
+	private function delete_image($filename) {
+		if ($this->use_azure) {
+
+		}
+		else {
+			@unlink($this->upload_path . '/' . $filename);
+		}
+	}
+
+	private function move_uploaded_file($tmp_name, $destination) {
+		if ($this->use_azure) {
+			return $this->upload_to_azure($tmp_name, $destination);
+		}
+		else {
+			return move_uploaded_file($tmp_name, $this->upload_path . '/' . $destination);
+		}
+	}
+
+	private function getBlobRestProxy() {
+		if (!$this->blobRestProxy) {
+			$connectionString = Helium::conf('azure_storage_connection_string');
+			$this->blobRestProxy = ServicesBuilder::getInstance()->createBlobService($connectionString);
+		}
+
+		return $this->blobRestProxy;
+	}
+
+	private function save_image($canvas, $filename) {
+		if ($this->use_azure) {
+			// Azure save magic here
+			$temp_filename = tempnam(sys_get_temp_dir(), Helium::conf('app_name'));
+			$temp_save = @imagejpeg($canvas, $temp_filename);
+			if ($temp_save) {
+				// Proceed to uploading to blob storage
+				$save_cloud = $this->upload_to_azure($temp_filename, $filename);
+				return $save_cloud;
+			}
+			else {
+				return false;
+			}
+		}
+		else {
+			$save = @imagejpeg($canvas, $this->upload_path . '/' . $filename);
+			return $save;
+		}
+	}
+
+	private function upload_to_azure($original_filename, $destination_filename) {
+		$connectionString = Helium::conf('azure_storage_connection_string');
+		$container = Helium::conf('azure_storage_picture_container');
+
+		$blobRestProxy = $this->getBlobRestProxy();
+		$content = fopen($original_filename, 'r');
+		$blob_name = $destination_filename;
+
+		try {
+			$blobRestProxy->createBlockBlob($container, $blob_name, $content);
+		}
+		catch (ServiceException $e) {
+			$code = $e->getCode();
+			$error_message = $e->getMessage();
+			var_dump($container);
+			echo $code . ': ' . $error_message;
+			return false;
+		}
 
 		return true;
 	}
@@ -189,8 +291,8 @@ class Picture extends HeliumRecord {
 	}
 
 	public function get_default_crop_dimensions() {
-		$original_full_path = $this->upload_path . '/' . $this->original_filename;
-		list($width, $height, $type, $attr) = getimagesize($original_full_path);
+		$width = $this->original_width;
+		$height = $this->original_height;
 
 		// get default crop dimensions
 		$ideal = compact('width', 'height');
