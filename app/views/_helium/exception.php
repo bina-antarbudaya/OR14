31e12b9432ba4851ec4a233f30b4160652b9533c
Andhika Nugraha <andhika.nugraha@outlook.com>
2014-02-06 23:23:13 +0000

    Made production error page more formal

--- app/views/_helium/exception.php
+++ app/views/_helium/exception.php
@@ -1,13 +1,14 @@
 <?php
 
-file_put_contents(HELIUM_PARENT_PATH . '/astaga.log', '[' . date('c') . '] ' . memory_get_usage() . ' ' . $_SERVER['REMOTE_ADDR'] . ' ' . $_SERVER['HTTP_USER_AGENT'] . ' '
-  . $_SERVER["REQUEST_URI"] . "\n", FILE_APPEND);
+// file_put_contents(HELIUM_PARENT_PATH . '/astaga.log', '[' . date('c') . '] ' . memory_get_usage() . ' ' . $_SERVER['REMOTE_ADDR'] . ' ' . $_SERVER['HTTP_USER_AGENT'] . ' '
+//   . $_SERVER["REQUEST_URI"] . "\n", FILE_APPEND);
 
 if (!function_exists('L')) {
 	function L($u) {
 		echo Helium::conf('base_uri') . $u;
 	}
 }
+
 ?>
 <!DOCTYPE html>
 
@@ -15,27 +16,31 @@ if (!function_exists('L')) {
 
 	<head>
 		<meta charset="utf-8">
-		<title>Pendaftaran Seleksi Bina Antarbudaya: Formulir Pendaftaran</title>
+		<title>Pendaftaran Seleksi Bina Antarbudaya</title>
 		<link rel="icon" href="<?php L('/assets/icon.png'); ?>" type="image/png">
-		<link rel="stylesheet" href="<?php L('/assets/css/global/style.css'); ?>">
-		<link rel="stylesheet" href="<?php L('/assets/css/global/error.css'); ?>">
+		<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,400italic,600,600italic:latin">
+		<link rel="stylesheet" href="<?php L('/assets/css/style.css'); ?>">
 	</head>
 
 	<body>
 		<header class="global-header">
 			<div class="container">
-				<header class="masthead"><a href="<?php L('/') ?>"><img src="https://www.seleksi.bina-antarbudaya.info/assets/css/global/masthead.png" alt="Bina Antarbudaya" width="226" height="40"></a></header>
+				<h1 class="branding">
+					<a href="<?php L('/') ?>">Bina Antarbudaya</a>
+				</h1>
+			</div>
 		</header>
+
 		<div class="content">
-<header class="page-title">
-		<h1>Astaga!</h1>
-</header>
-<div class="container">
-	
-	<p>Terjadi sebuah kesalahan teknis. Kami akan segera memeriksanya.</p>
-	<p>Tweet tentang masalah ini dengan tagar #WaduhCurhat.</p>
-	<p><strong><a href="<?php echo $_SERVER['HTTP_REFERER'] ?>">Kembali ke halaman sebelumnya</a></strong></p>
+			<div class="container">
+				<header class="page-header">
+					<h1>Maaf</h1>
+				</header>
+				
+				<p>Terjadi sebuah kesalahan teknis. Kami akan segera memeriksa penyebabnya.</p>
+				<p><a class="btn btn-primary" href="<?php echo $_SERVER['HTTP_REFERER'] ?>">Kembali ke halaman sebelumnya</a></p>
+			</div>
 		</div>
-			</body>
+	</body>
 
 </html>
