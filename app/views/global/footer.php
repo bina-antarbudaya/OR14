f0edf838f374ad78844b8f3de6cdf435f0314703
Andhika Nugraha <andhika.nugraha@outlook.com>
2014-02-25 16:55:32 +0000

    Bumped jQuery to 1.11.0
    * Use Microsoft's CDN
    * Use jQuery migrate

--- app/views/global/footer.php
+++ app/views/global/footer.php
@@ -1,12 +1,12 @@
 			</div>
 		</div>
-		
+
 		<footer class="global-footer">
 		</footer>
-		
-		
+
 		<!-- Load JS -->
-	    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+	    <script src="//ajax.aspnetcdn.com/ajax/jQuery/jquery-1.11.0.min.js"></script>
+	    <script src="//ajax.aspnetcdn.com/ajax/jquery.migrate/jquery-migrate-1.2.1.min.js"></script>
 	   	<script src="<?php L('assets/js/bootstrap.min.js') ?>"></script>
 		<?php foreach ($this->additional_js as $js): ?>
 	 	<script src="<?php L('assets/js/' . $js . '.js') ?>"></script>
